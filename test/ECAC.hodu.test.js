5d18b5926d0a46789c80b8581622e7232f79e0c3
unknown <jungmin011107@gmail.com>
2023-07-18 17:47:47 +0900

    함수 별 이더리움 송금 트랜잭션 실행 및 시간 확인

--- test/ECAC.hodu.test.js
+++ test/ECAC.hodu.test.js
@@ -19,12 +19,13 @@ moment.tz.setDefault("Asia/Seoul");
 
 let accounts = [];
 // likes와 reflectedLikes 지정
-let likes = 421;
+let likes = 701;
+let reflectedLikes = 0;
 
 // nestedComments와 Count지정
-let nestedComments = 21;
+let nestedComments = 61;
 
-let account, account3, account4, account5, account8;
+let account;
 let address;
 
 // 포인트 검증하기 위한 변수
@@ -35,17 +36,18 @@ let input = 100;
 
 // 스마트 컨트랙트 받아오기 위한 변수
 let box;
-// 새로운 계좌 생성할 때 받아올 변수
-let newAccount;
+
+var eth_point = 0;
+var sender;
+var receiver;
 
 
 beforeEach(async () => {
+    const currentTime = moment().format("YYYY-MM-DD HH:mm:ss");
+    console.log("함수 실행 시작 시간: " + currentTime);
 
     accounts = await web3.eth.getAccounts();
-    account3 = accounts[3];
-    account4 = accounts[4];
-    account5 = accounts[5];
-    account8 = accounts[8];
+    account = accounts[3];
 
     // 주소 체크섬 오류 해결 -> 스마트컨트랙트 배포 주소
     address = web3.utils.toChecksumAddress('0x3b6d00DE1373ccbEbeA835FF76825DBB7F0bC7c4')
@@ -55,51 +57,124 @@ beforeEach(async () => {
         // compiledAbi,
         address
     );
-    await box.methods.addUser(account3).call();
-    await box.methods.addUser(account4).call();
-    await box.methods.addUser(account5).call();
-    await box.methods.addUser(account8).call();
+    await box.methods.addUser(account).call();
 
     // 주소 unlock
     web3.eth.personal.unlockAccount(accounts[0], '', 0);
     web3.eth.personal.unlockAccount(accounts[3], '', 0);
-    web3.eth.personal.unlockAccount(accounts[4], '', 0);
-    web3.eth.personal.unlockAccount(accounts[5], '', 0);
-    web3.eth.personal.unlockAccount(accounts[8], '', 0);
 
-});
-
-describe('[1: 계좌 생성]', () => {
-
-    it('계좌 생성하기', async () => {        
-        try {
-            newAccount = web3.eth.accounts.create();
-            console.log('생성 성공');
+    // try-catch문으로 모든 것이 실행되기 전에 시나리오에서처럼 포인트가 얼마 있는지 확인해보면 된다.
+    await box.methods.getCount(account).call((error, result) => {
+        if (error) {
+            console.error('Error:', error);
+        } else {
+            console.log('Account3 has :', result);
         }
-        catch (err) {
-            console.error('생성 실패:', err);
-        }
-        // 새로 생성한 계좌 정보
-        console.log('New Account Address:', newAccount.address);
-        console.log('New Account Private Key:', newAccount.privateKey);
     });
 });
 
+// 계좌 3 좋아요
+describe('[1: 계좌/유저 1개에 대한 좋아요 포인트 지급 함수 실행]', () => {
 
+    it('2. Is the function addPoint_likes() running correctly?', async () => {
+        let point_like1 = await box.methods.getCount(account).call();
+        console.log("User's Point before addPoint_likes() is : " + point_like1);
+        try {
+            await box.methods.addPoint_likes(account, likes).send({
+                from: accounts[0],
+                gas: 1000000
+            }).then((receipt) => {
+                // console.log("Transaction receipt: ", receipt);
+                console.error("addPoint_likes SUCCESS");
+            }).catch((error) => {
+                console.error("Error: ", error);
+            });
+        }
+        catch (err) {
+            console.log(err.message);
+        }
+        let point_like2 = await box.methods.getCount(account).call();
+        console.log("User's Point after addPoint_likes() is : " + point_like2);
+        eth_point = 0.001 * (point_like2 - point_like1);
+        sender = accounts[0];
+        receiver = account;
+    });
+});
 
-describe('[2: 송금]', () => {
+// 계좌 3 대댓글
+describe('[2: 동일 계좌/유저 1개에 대한 대댓글 포인트 지급 함수 실행]', () => {
 
-    it('송금 메서드 작성', async () => {
-        const amountToSend = web3.utils.toWei('0.5', 'ether');
-        var balance1 = await web3.eth.getBalance(newAccount.address);
-        console.log("Get Receipient Balance Before : " + balance1);
-        var balance2 = await web3.eth.getBalance(accounts[0]);
-        console.log("Get Sender Balance Before : " + balance2);
+    it('3.1. Is the function addPoint_nestedComments() running correctly?', async () => {
+        let point_nestedComment1 = await box.methods.getCount(account).call();
+        console.log("User's Point before addPoint_nestedComments()" + point_nestedComment1);
+        try {
+            // 대댓글이 추가되었을 때 실행하면 사용자에게 100포인트를 지급해주는 함수 
+            await box.methods.addPoint_nestedComments(account, nestedComments).send({
+                from: accounts[0],
+                gas: 1000000
+            }).then((receipt) => {
+                // console.log("Transaction receipt: ", receipt);
+                console.error("addPoint_nestedComments SUCCESS");
+            }).catch((error) => {
+                console.error("Error: ", error);
+            });
+            result = await box.methods.getCount(account).call();
+            // console.log("The number of result: ", result);
+        }
+        catch (err) {
+            console.log(err.message);
+        }
+        let point_nestedComment2 = await box.methods.getCount(account).call();
+        console.log("User's Point after addPoint_nestedComments()" + point_nestedComment2);
+        eth_point = 0.001 * (point_nestedComment2 - point_nestedComment1);
+        sender = accounts[0];
+        receiver = account;
+    });
+});
+
+// 계좌 3 포인트 환급
+describe('[3: 동일 계좌/유저 1개에 대한 포인트 환급 함수 실행]', () => {
+    it('4. Is the function refunds() running correctly?', async () => {
+        let point = await box.methods.getCount(account).call();
+        console.log("User's Point before refunds() " + point);
+        try {
+            // refund를 실행하면 전체의 포인트에서 입력한 수만큼의 포인트를 빼주게 됨 
+            await box.methods.refunds(account, input).send({
+                from: accounts[0],
+                gas: 1000000
+            }).then((receipt) => {
+                // console.log("Transaction receipt: ", receipt);
+                console.error("refunds SUCCESS");
+            }).catch((error) => {
+                console.error("Error: ", error);
+            });
+
+        }
+        catch (err) {
+            console.log(err.message);
+        }
+        let left_point = await box.methods.getCount(account).call();
+        console.log("User's Point after refunds() " + left_point);
+        eth_point = 0.001 * (point - left_point);
+        sender = account;
+        receiver = accounts[0];
+    });
+});
+
+afterEach(async () => {
+    if (eth_point != 0) {
+        const amount = web3.utils.toWei(eth_point.toString(), 'ether');
+        var balance1 = await web3.eth.getBalance(sender);
+        var balance1InEther = web3.utils.fromWei(balance1, 'ether');
+        console.log("Get Sender Balance Before : " + balance1InEther);
+        var balance2 = await web3.eth.getBalance(receiver);
+        var balance2InEther = web3.utils.fromWei(balance2, 'ether');
+        console.log("Get Receipient Balance Before : " + balance2InEther);
         try {
             const transact = {
-                from: accounts[0],
-                to: newAccount.address,
-                value: amountToSend,
+                from: sender,
+                to: receiver,
+                value: amount,
             };
             const receipt = await web3.eth.sendTransaction(transact);
             console.log('송금 완료');
@@ -107,9 +182,14 @@ describe('[2: 송금]', () => {
         catch (err) {
             console.error('송금 실패:', err);
         }
-        balance1 = await web3.eth.getBalance(newAccount.address);
-        console.log("Get Receipient Balance After : " + balance1);
-        balance2 = await web3.eth.getBalance(accounts[0]);
-        console.log("Get Sender Balance After : " + balance2);
-    });
+        balance1 = await web3.eth.getBalance(sender);
+        balance1InEther = web3.utils.fromWei(balance1, 'ether');
+        console.log("Get Sender Balance After : " + balance1InEther);
+        balance2 = await web3.eth.getBalance(receiver);
+        balance2InEther = web3.utils.fromWei(balance2, 'ether');
+        console.log("Get Receipient Balance After : " + balance2InEther);
+    }
+    const currentTime = moment().format("YYYY-MM-DD HH:mm:ss");
+    console.log("함수 실행 끝난 시간: " + currentTime);
+
 });
